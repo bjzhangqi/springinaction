fa870b387743104e384ef6d0e9e7ee33a378e370
bjyfzhangqi <bjyfzhangqi@jd.com>
2019-02-12 18:13:16 +0800

    first commit

---
+++ src/main/java/com/springinaction/knights/SlayDragonQuest.java
@@ -0,0 +1,17 @@
+package com.springinaction.knights;
+
+import java.io.PrintStream;
+
+public class SlayDragonQuest implements Quest {
+
+    private PrintStream stream;
+
+    public SlayDragonQuest(PrintStream stream) {
+        this.stream = stream;
+    }
+
+    @Override
+    public void embark() {
+        stream.println("Embarkingh on quest to slay the dragon");
+    }
+}
